9217064c62db0da2b31f6dae34125169559cd0b5
Brian Pompey <brianthescholar@gmail.com>
2019-11-24 22:24:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
 
   end
 
