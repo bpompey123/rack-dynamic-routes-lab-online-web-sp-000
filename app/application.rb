600d6a8daf03b90bedb7170b009018ef5c563e10
Brian Pompey <brianthescholar@gmail.com>
2019-11-24 22:26:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,10 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      
+      if item = @@items.find{|i| i.name = item_name}
+        resp.write item.price
+      else
+        resp.status = 400
 
   end
 
